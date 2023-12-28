c9e4a3e7caeeeb3514d8cc70d5bc48aefed4eec8
Gaurav3697 <gm4063420@gmail.com>
2023-12-28 14:40:41 +0545

    learnt mongodb

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 import express from "express";
 import path from "path";
+import mongoose from "mongoose";
 
+mongoose.connect("mongodb://127.0.0.1:27017",{      //go to mongo in programm file and type mongod in cmd then go to mongosh file and type mongo to get mongodbURL
+    dbName:"backend",
+}).then(()=>console.log("Database conected"))
+.catch((e)=>console.log(e));
+
+const messageSchema = new mongoose.Schema({
+    name:String ,
+    email: String,
+})
+
+const message = mongoose.model("Message",messageSchema);
 const app = express();            //creating server
 
 //using middlewares
@@ -15,8 +27,9 @@ app.get("/success",(req,res)=>{
     res.render("success.ejs",{name:"Gaurav"});     //using ejs
 })
 
-app.post("/",(req,res)=>{
-    console.log(req.body);
+app.post("/",async(req,res)=>{
+    const {firstname ,email} =req.body    //destucturing firstname and email from req.body
+    await message.create({name:firstname , email: email});
     res.redirect("/success")
 });
 
